108e01292f4bfcc07fb6866db23995b0bdbc6cd3
Prajjwal Datir <prajjwaldatir0072000@gmail.com>
2022-02-16 00:28:07 +0530

    Overview

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import './App.css';
+import Form from './components/Form';
+import Header from './components/Header';
+import QuickWeather from './components/QuickWeather';
 
 // Weather App init
 function App() {
   return (
     <>
-      <h1>Weatherly : Your Friendly Neighbourhood Weather App</h1>      
+      <Header></Header>
+      <QuickWeather></QuickWeather>
     </>
   );
 }
